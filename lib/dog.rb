955d3620ed72012eb14101dbe0074be0055dbedb
Ryan Parrish <rino3717@gmail.com>
2020-03-30 17:38:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+def name=(dog_name)
+  @this_dog_name = dog_name
+end
+
+def name
+  @this_dog_name
+end
+
+def breed=(dog_breed)
+  @this_dog_breed = dog breed
+end
+
+def breed 
+  @this_dog_breed
+end
+
+end
+  
+  
